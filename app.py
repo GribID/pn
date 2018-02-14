98458e26709e5c8d614bbe8677e942017d24424c
Igor Grib <grib.id@gmail.com>
2018-02-14 14:59:27 +0300

    Add sql

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import pyodbc
 
 app = Flask(__name__)
@@ -6,11 +6,29 @@ app = Flask(__name__)
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=10.120.52.101;DATABASE=Patio;UID=it;PWD=it')
 cursor = cnxn.cursor()
 
+
 @app.route('/')
-def hello_world():
-    cursor.execute('SELECT * FROM LV_Depositor')
-    row1 = cursor.fetchall()
-    return str(row1)
+def home():
+    return render_template('index.html', title='main')
+
+
+@app.route('/order/')
+def order():
+    cursor.execute('SELECT * '
+                   'FROM LV_Order '
+                   'WHERE ord_InputDate > GETDATE()-1'
+                   'ORDER BY ord_InputDate DESC')
+    return render_template('order.html', title='database', items=cursor.fetchall())
+
+
+@app.route('/order/<ord_code>')
+def order_detail(ord_code):
+    cursor.execute('SELECT * '
+                   'FROM LV_OrderItem INNER JOIN '
+                   'LV_ProductLang ON LV_OrderItem.ori_ProductID = LV_ProductLang.prdl_ProductID '
+                        'AND LV_ProductLang.prdl_LanguageID = 4 '
+                   'WHERE ori_OrderID = ' + ord_code)
+    return render_template('order_detail.html', title='order', items=cursor.fetchall())
 
 
 if __name__ == '__main__':
