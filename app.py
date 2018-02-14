c04c1e6e8310fbc21d100cdc5a356e13b5fe43ed
Igor Grib <grib.id@gmail.com>
2018-02-14 13:02:29 +0300

    Add sql

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+import pyodbc
 
 app = Flask(__name__)
 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=10.120.52.101;DATABASE=Patio;UID=it;PWD=it')
+cursor = cnxn.cursor()
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    cursor.execute('SELECT * FROM LV_Depositor')
+    row1 = cursor.fetchall()
+    return str(row1)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
