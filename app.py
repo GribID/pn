fb7239493a81c29eed291a986c86f853e87e26bc
Igor Grib <grib.id@gmail.com>
2018-02-22 16:30:56 +0300

    Change db connect

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template, request
-import pyodbc
 import sql
 
 app = Flask(__name__)
-
-#cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=10.120.52.101;DATABASE=Patio;UID=it;PWD=it')
-cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=10.120.52.101;DATABASE=Patio;UID=it;PWD=it')
-cursor = cnxn.cursor()
 
 
 @app.route('/')
@@ -16,22 +11,22 @@ def home():
 
 @app.route('/order/')
 def order():
-    cursor.execute(sql.order)
-    return render_template('order.html', title='database', items=cursor.fetchall())
+    sn = sql.zz(sql.order)
+    return render_template('order.html', title='database', items=sn.fetchall())
 
 
 @app.route('/order/<ord_code>')
 def order_detail(ord_code):
-    cursor.execute(sql.order_item + ord_code)
-    return render_template('order_detail.html', title='order', items=cursor.fetchall())
+    sn = sql.zz(sql.order_item + ord_code)
+    return render_template('order_detail.html', title='order', items=sn.fetchall())
 
 
 @app.route('/task/', methods=['GET', 'POST'])
 def task():
     if request.method == 'POST':
-         return str(request.form['mindate'])
+        return str(request.form['mindate'])
 
-    d = cursor.execute(sql.dep)
+    d = sql.zz(sql.dep)
     return render_template('task.html', title='task', items=d.fetchall())
 
 
--- sql.py
+++ sql.py
@@ -1,3 +1,13 @@
+import pyodbc
+
+
+def zz(sqlreq):
+#   cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=10.120.52.101;DATABASE=Patio;UID=it;PWD=it')
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=10.120.52.101;DATABASE=Patio;UID=it;PWD=it')
+    cursor = cnxn.cursor()
+    return cursor.execute(sqlreq)
+
+
 dep = '''
 SELECT * 
 FROM LV_Depositor
